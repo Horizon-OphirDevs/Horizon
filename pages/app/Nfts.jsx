1b5fc821ce3106bd320e0684d983db017eec518a
SKALEZ-A <holumacbeth@gmail.com>
2023-08-17 13:00:19 +0100

    added styles

--- pages/app/Nfts.jsx
+++ pages/app/Nfts.jsx
@@ -3,7 +3,12 @@ import React from "react";
 const Nfts = () => {
   return (
     <div>
-      <h1>Nfts</h1>
+      <h1 className="text-gray-300">Nfts</h1>
+      <div className="white-glassmorphism flex align-center justify-center">
+        <p className="text-gray-300 text-3xl flex align-center m-auto">
+          COMING SOON
+        </p>
+      </div>
     </div>
   );
 };
