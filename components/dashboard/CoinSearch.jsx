fca7fcfb04c8348c72125e0b0fdd1e6981ab19db
yomidepgit config --global user.name yomidepgit config --global user.name yomidep <noabiliaminfirst@gmail.com>
2023-09-14 10:05:40 +0100

    Search function fixed

--- components/dashboard/CoinSearch.jsx
+++ components/dashboard/CoinSearch.jsx
@@ -1,99 +1,97 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function CoinSearch() {
+  const [query, setQuery] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
   const [isDialogOpen, setDialogOpen] = useState(false);
+  const [selectedCoin, setSelectedCoin] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
 
-  const openDialog = () => {
+  const handleInputChange = (event) => {
+    setQuery(event.target.value);
+  };
+
+  const fetchData = async () => {
+    setIsLoading(true);
+    try {
+      const response = await fetch(`/api/search?query=${query}`);
+      const data = await response.json();
+      setSearchResults(data);
+    } catch (error) {
+      console.error("Error fetching data from API:", error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const openDialog = (coin) => {
+    setSelectedCoin(coin);
     setDialogOpen(true);
   };
 
   const closeDialog = () => {
+    setSelectedCoin(null);
     setDialogOpen(false);
   };
 
+  useEffect(() => {
+    if (query) {
+      fetchData();
+    }
+  }, [query]);
+
   return (
     <>
-      <div className="relative border flex md:w-full bg-[#1f1f1f] items-stretch rounded-lg">
+      <div className="border flex md:w-full bg-[#1f1f1f] items-stretch rounded-lg">
         <input
           type="search"
-          className="relative m-0 block md:w-52 min-w-0 flex-auto rounded bg-transparent bg-clip-padding px-3 py-[0.25rem] text-xs font-normal leading-[1.6] text-gray-500 outline-none transition duration-200 ease-in-out focus:z-[3] focus:border-primary focus:text-gray-400 focus:shadow-[inset_0_0_0_1px_rgb(59,113,202)] focus:outline-none dark:border-neutral-600 dark:focus:border-primary"
+          className="block md:w-52 min-w-0 flex-auto rounded bg-transparent bg-clip-padding px-3 py-[0.25rem] text-xs font-normal leading-[1.6] text-gray-500 outline-none transition duration-200 ease-in-out focus:z-[3] focus:border-primary focus:text-gray-400 focus:shadow-[inset_0_0_0_1px_rgb(59,113,202)] focus:outline-none dark:border-neutral-600 dark:focus:border-primary"
           placeholder="Search token or contract"
-          onClick={openDialog}
+          value={query}
+          onChange={handleInputChange}
         />
-        <span
-          className="input-group-text flex items-center whitespace-nowrap rounded px-3 py-1.5 text-center text-base font-normal text-gray-700 dark:text-neutral-200 cursor-pointer"
-          aria-label="Search"
-        >
-          {/* Search icon */}
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-            className="h-5 w-5"
-          >
-            <path
-              fillRule="evenodd"
-              d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z"
-              clipRule="evenodd"
-            />
+        <span className="input-group-text flex items-center whitespace-nowrap rounded px-3 py-1.5 text-center text-base font-normal text-gray-700 dark:text-neutral-200 cursor-pointer" aria-label="Search">
+          {/* Placeholder SVG path for search icon */}
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="h-5 w-5">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-5.2-5.2"></path>
+            <circle cx="10" cy="10" r="7"></circle>
           </svg>
         </span>
       </div>
 
-      {/* Dialog */}
-      {isDialogOpen && (
-        <div
-          class="relative z-10"
-          aria-labelledby="modal-title"
-          role="dialog"
-          aria-modal="true"
-        >
-          <div class="fixed inset-0 bg-[#101010] bg-opacity-75 transition-opacity"></div>
+      {/* Display search results */}
+      {searchResults.length > 0 && (
+        <div className="dialog text-white">
+          <div className="dialog-content">
+            <h2 className="text-2xl font-semibold mt-4">Search Results</h2>
+            {isLoading ? (
+              <p>Loading...</p>
+            ) : (
+              <ul>
+                {searchResults.map((result, index) => (
+                  <li key={index}>
+                    {/* Render search result data here */}
+                    <p>Name: {result.name}</p>
+                    <p>Market Cap Rank: {result.market_cap_rank}</p>
+                    <button onClick={() => openDialog(result)}>Open Details</button>
+                  </li>
+                ))}
+              </ul>
+            )}
+            <button onClick={closeDialog}>Close</button>
+          </div>
+        </div>
+      )}
 
-          <div class="fixed inset-0 z-10 w-screen overflow-y-auto">
-            <div class="flex  items-end justify-center p-4 text-center sm:items-center sm:p-0">
-              <div class="relative transform overflow-hidden rounded-lg bg-[#1f1f1f] text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
-                <div class="bg-[#1f1f1f] px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
-                  <div class="sm:flex sm:items-start">
-                    <div class="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
-                      <svg
-                        class="h-6 w-6 text-red-600"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke-width="1.5"
-                        stroke="currentColor"
-                        aria-hidden="true"
-                      >
-                        <path
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z"
-                        />
-                      </svg>
-                    </div>
-                    <div class="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
-                      <h3
-                        class="text-base leading-6 text-gray-300"
-                        id="modal-title"
-                      >
-                        Sorry, our search feature is currently unavailable.
-                        <br />
-                        Please explore our website in the meantime. Thank you
-                      </h3>
-                    </div>
-                  </div>
-                </div>
-                <div class="bg-[#1f1f1f] px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
-                  <button
-                    onClick={closeDialog}
-                    type="button"
-                    class="mt-2 inline-flex w-full justify-center rounded-md bg-gray-500 px-3 py-2 text-sm font-semibold text-gray-300 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
-                  >
-                    Cancel
-                  </button>
-                </div>
-              </div>
-            </div>
+      {/* Display the selected coin in a dialog */}
+      {isDialogOpen && selectedCoin && (
+        <div className="dialog">
+          <div className="dialog-content">
+            <h2>{selectedCoin.name}</h2>
+            <img src={selectedCoin.thumb} alt={selectedCoin.name} />
+            {/* Additional details here */}
+            <p>Market Cap Rank: {selectedCoin.market_cap_rank}</p>
+            <button onClick={closeDialog}>Close</button>
           </div>
         </div>
       )}
--- pages/api/search.js
+++ pages/api/search.js
@@ -3,7 +3,7 @@ export default async function handler(req, res) {
     return res.status(405).json({ error: 'Method not allowed' });
   }
 
-  const { query } = req.query;
+  const query = req.query.query; // Extract the 'query' parameter from the query string
 
   if (!query) {
     return res.status(400).json({ error: 'Missing query parameter' });
@@ -12,7 +12,15 @@ export default async function handler(req, res) {
   try {
     const response = await fetch(`https://api.coingecko.com/api/v3/search?query=${query}`);
     const data = await response.json();
-    res.status(200).json(data);
+
+    // Extract the relevant information from the response
+    const coins = data.coins.map((coin) => ({
+      name: coin.name,
+      thumb: coin.thumb,
+      marketCapRank: coin.market_cap_rank,
+    }));
+
+    res.status(200).json(coins);
   } catch (error) {
     console.error('Error fetching data from CoinGecko API:', error);
     res.status(500).json({ error: 'Internal server error' });
