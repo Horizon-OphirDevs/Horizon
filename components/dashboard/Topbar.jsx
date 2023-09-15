00c251a31a882b124c7995ec0d63f7e0fb225797
SKALEZ-A <holumacbeth@gmail.com>
2023-09-15 07:34:23 +0100

    next

--- components/dashboard/Topbar.jsx
+++ components/dashboard/Topbar.jsx
@@ -125,33 +125,6 @@ const Topbar = ({ activeSection }) => {
                   <ConnectWallet btnTitle="+ Connect Wallet" />
                 </div>
 
-                <div>
-                  <div class="relative border flex mb-2  md:w-full md:hidden bg-[#1f1f1f] items-stretch rounded-lg">
-                    <input
-                      onClick={openDialog}
-                      type="search"
-                      className="relative m-0 block md:w-52 min-w-0  flex-auto rounded bg-[#1f1f1f] bg-clip-padding px-3 py-[0.25rem] text-xs font-normal leading-[1.6] text-neutral-700 outline-none transition duration-200 ease-in-out focus:z-[3] focus:border-primary focus:text-neutral-700 focus:shadow-[inset_0_0_0_1px_rgb(59,113,202)] focus:outline-none dark:border-neutral-600 dark:focus:border-primary"
-                      placeholder="search token or contract "
-                    />
-                    {/* <!--Search icon--> */}
-                    <span class="input-group-text flex items-center whitespace-nowrap rounded px-3 py-1.5 text-center text-base font-normal text-neutral-700 dark:text-neutral-200">
-                      {" "}
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 20 20"
-                        fill="currentColor"
-                        class="h-5 w-5"
-                      >
-                        <path
-                          fill-rule="evenodd"
-                          d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z"
-                          clip-rule="evenodd"
-                        />
-                      </svg>
-                    </span>
-                  </div>
-                </div>
-
                 <Link
                   href="/app/Portfolio"
                   onClick={() => setActiveSection("Dashboard")}
